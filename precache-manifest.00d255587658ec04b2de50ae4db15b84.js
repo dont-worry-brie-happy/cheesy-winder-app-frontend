22914afc527b1788c6b2f9418e44ba1a512856ba
HeatherJLewis <heather@heatherlewis.co.uk>
2019-03-06 08:04:54 +0000

    Updates

---
+++ precache-manifest.00d255587658ec04b2de50ae4db15b84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd6ac9a717c90cd867c48539c936d85e",
+    "url": "/cheesy-winder-app-frontend/static/media/brie.fd6ac9a7.png"
+  },
+  {
+    "revision": "af5ee60ad6d9b5525225",
+    "url": "/cheesy-winder-app-frontend/static/js/runtime~main.af5ee60a.js"
+  },
+  {
+    "revision": "e81cc67f0d9474c19067",
+    "url": "/cheesy-winder-app-frontend/static/js/main.e81cc67f.chunk.js"
+  },
+  {
+    "revision": "5cda3d31cb8cf9bfd16b",
+    "url": "/cheesy-winder-app-frontend/static/js/2.5cda3d31.chunk.js"
+  },
+  {
+    "revision": "e81cc67f0d9474c19067",
+    "url": "/cheesy-winder-app-frontend/static/css/main.8e4b58ea.chunk.css"
+  },
+  {
+    "revision": "229f81392488df98b516ec7eacb64a57",
+    "url": "/cheesy-winder-app-frontend/index.html"
+  }
+];
